1dae964819f434562951226178a3632806a5bf1b
ChanidaphaBoonyubon <pan025711174@gmail.com>
2026-02-28 17:12:39 +0700

    upload

--- About/about2.js
+++ About/about2.js
@@ -134,3 +134,16 @@ contactTab.forEach(tab => {
   openContactTab(defaultTab);
 
 });
+document.addEventListener('DOMContentLoaded', function() {
+
+    const toggleHeader = document.querySelector('.careerH');
+    const contentSection = document.querySelector('.career');
+
+    
+    toggleHeader.addEventListener('click', function() {
+    
+        contentSection.classList.toggle('show');
+        
+        toggleHeader.classList.toggle('active');
+    });
+});
